246d5b26d219a47ddec1a729f80acb4b833ba9af
sasindumalshan <sasindu.malshan03262001@gmail.com>
2025-04-09 00:03:53 +0530

    print variables in rust

--- hello_world.rs
+++ hello_world.rs
@@ -3,13 +3,37 @@
  * This is the main() function which acts as an entry point of every Rust program. It is always the first code that runs in every Rust program.
  * The body of the function is wrapped inside curly brackets, {}. We will learn more about functions in later chapters.
  * 
+ * Here, print! is a macro that prints the text inside double quotes. To learn more about macros, visit Rust Macro.
+ *
+ * 
+ * In Rust, there are two variations of the print:
+ *
+ *      1. print!()
+ *      2. println!()
+ * 
  */
 fn main(){
-    println!("=========== Difference between print and println ================\n");
-    println!("===========  print ================ \n");
+   println!("=========== Difference between print and println ================\n");
+   println!("===========  print ================ \n");
    print();
+
    println!("\n\n===========  println ================ \n");
    println();
+
+   println!("\n\n===========  Print Variables ================ \n");
+   print_variables();
+}
+
+fn print_variables(){
+    let age = 31;
+    let name = "Jack";
+    println!("Age: {}, name: {}", age, name);
+    // output: Age: 31, name: Jack
+    println!("Age: {1}, name: {0}", age, name);
+    // output: Age: Jack, name: 31
+    println!("Age: {age}, name: {name}");
+    // output: Age: 31, name: Jack
+
 }
 
 fn print(){
@@ -35,21 +59,3 @@ fn println(){
     * Hello, World!
     */
 }
-
-
-/*
- * full Output:
- * 
- * =========== Difference between print and println ================
- * 
- * ===========  print ================ 
- * 
- * Hello, World!Hello, World!Hello, World!
- * 
- * ===========  println ================ 
- * 
- * Hello, World!
- * Hello, World!
- * Hello, World!
- * 
- */
